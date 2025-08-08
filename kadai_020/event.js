24dcf460b6b05dd5b6860566e274d850c680e63c
kimuratatsuki4-cmd <kimuratatsuki4@gmail.com>
2025-08-09 00:28:39 +0900

    修正分をコミット

--- kadai_020/event.js
+++ kadai_020/event.js
@@ -1,7 +1,7 @@
 // ボタンがクリックされたときの処理を追加
 const btn = document.getElementById('btn');
-
+const text = document.getElementById('text');
+// イベント処理
 btn.addEventListener('click', () => {
-    const text = document.getElementById('text');
     text.textContent = 'ボタンをクリックしました';
 });
