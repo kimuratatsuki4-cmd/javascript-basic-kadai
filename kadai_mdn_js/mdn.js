e358d1f39d41494da47696bd07c5348bbb58815c
kimuratatsuki4-cmd <kimuratatsuki4@gmail.com>
2025-08-08 21:37:56 +0900

    再提出

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,3 +1,3 @@
-// dateクラスを利用して「2024年10月12日」と出力するコードを記載する
-const date = new Date(2024, 9, 12);
+// dateクラスを利用して本日の日付を出力するコードを記載する
+const date = new Date();
 console.log(date.toLocaleDateString("ja-JP", { year: 'numeric', month: 'long', day: 'numeric' }));
